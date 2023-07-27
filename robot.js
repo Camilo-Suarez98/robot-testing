1e82afd0d2f3b8fdd286e4c741324f00cd253d4e
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-07-26 23:08:39 -0500

    fixing test

--- robot.js
+++ robot.js
@@ -1,28 +1,96 @@
-function createRobot(coordinates, direction) {
-  let [x, y] = coordinates
-  let orientation = direction 
+function createRobot([_x, _y], direction) {
+  let x = _x
+  let y = _y
+  let orientation = direction
+  let rightOrientation = [ 'North', 'East', 'South', 'West' ]
+  let message
+  
+  (function validator() {
+    try {
+      const isValid = rightOrientation.includes(orientation)
+      if (!isValid) {
+        throw new Error('invalid orientation')
+      }
 
-  return {
-    getPosition: () => {
+      if (x > 10 || y > 10 || x < 0 || y < 0) {
+        throw new Error('invalid coordinates')
+      }
+
+      message = 'Robot created succesfully'
+    } catch (error) {
+      message = error.message
+    }
+  })()
+
+  function getPosition () {
+    try {
+      if (x > 10 || y > 10 || x < 0 || y < 0) {
+        return ('Out of limits')
+      }
+
       return { coordinates: [x, y], orientation }
-    },
-    advance: () => {
-      // función para avanzar
-    },
-    turnRight: () => {
-      // función para girar a la derecha
-    },
-    turnLeft: () => {
-      // función para girar a la izquierda
-    },
-    instructions: (stringInstructions) => {
-      // función para recibir una serie de instrucciones Ejm: "RAALAL"
-      // En este metodo se va crear 
+    } catch (error) {
+      message = error.message
     }
   }
+
+  function advance () {
+    if (orientation === 'North') {
+      y += 1
+    } else if (orientation === 'East') {
+      x += 1
+    } else if (orientation === 'South') {
+      y -= 1
+    } else if (orientation === 'West') {
+      x -= 1
+    }
+  }
+
+  function turnRight () {
+    if (orientation === 'North') {
+      orientation = 'East'
+    } else if (orientation === 'East') {
+      orientation = 'South'
+    } else if (orientation === 'South') {
+      orientation = 'West'
+    } else {
+      orientation = 'North'
+    }
+  }
+
+  function turnLeft ()  {
+    if (orientation === 'North') {
+      orientation = 'West'
+    } else if (orientation === 'East') {
+      orientation = 'North'
+    } else if (orientation === 'South') {
+      orientation = 'East'
+    } else if (orientation === 'West') {
+      orientation = 'South'
+    }
+  }
+
+  function instructions (string) {
+    for (let i = 0; i < string.length; i ++) {
+      if (string[i] === 'R') {
+        turnRight()
+      } else if (string[i] === 'L') {
+        turnLeft()
+      } else if (string[i] === 'A') {
+        advance()
+      }
+    }     
+  }
+
+  return { 
+    message,
+    getPosition,
+    advance,
+    turnRight,
+    turnLeft,
+    instructions
+  }
+  
 }
-
+  
 module.exports = createRobot
-
-
-console.log(createRobot([2,3], "North").getPosition());
--- robot.test.js
+++ robot.test.js
@@ -1,7 +1,255 @@
-const { createRobot } = require('./robot.js');
+const  createRobot = require('./robot.js');
 
-describe('Robot', () => {
-	test('Cambia el titulo y empieza a escribir tu primera prueba', () => {
+describe('createRobot', () => {
+	test('Should return error with message invalid orientation', () => {
+		const message = 'invalid orientation'
+    const myRobot = createRobot([4, 3], 'Soth')
+		expect(myRobot.message).toBe(message)
+	});
 
+	test('Should return error with message invalid coordinates', () => {
+		const message = 'invalid coordinates'
+		const myRobot = createRobot([11, 0], 'East')
+		expect(myRobot.message).toBe(message)
+	});
+
+	test('Should return Robot created succefully', () => {
+		const message = 'Robot created succesfully'
+		const myRobot = createRobot([4, 3], 'South')
+		expect(myRobot.message).toBe(message)
 	});
 });
+
+describe('getPosition from robot', () => {
+	test('Should return error with message out of limits', () => {
+		const message = 'Out of limits'
+		const myRobot = createRobot([11, 1], 'North')
+		const result = myRobot.getPosition()
+		expect(result).toBe(message)
+	})
+
+	test('Should return with place succefully', () => {
+		const place =  {coordinates: [7, 4], orientation: 'East'}
+		const myRobot = createRobot([7, 4], 'East')
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+})
+
+describe('robot advance', () => {
+	test('Should advance with coordinates: [5, 2], orientation: West', () => {
+		const place =  { coordinates: [4, 2], orientation: 'West' }
+		const myRobot = createRobot([5, 2], 'West')
+		myRobot.advance()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with coordinates: [5, 2], orientation: East', () => {
+		const place =  { coordinates: [6, 2], orientation: 'East' }
+		const myRobot = createRobot([5, 2], 'East')
+		myRobot.advance()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with coordinates: [5, 2], orientation: South', () => {
+		const place =  { coordinates: [5, 1], orientation: 'South' }
+		const myRobot = createRobot([5, 2], 'South')
+		myRobot.advance()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with coordinates: [5, 2], orientation: North', () => {
+		const place =  { coordinates: [5, 3], orientation: 'North' }
+		const myRobot = createRobot([5, 2], 'North')
+		myRobot.advance()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+})
+
+describe('robot turn to right', () => {
+	test('Should advance to right with coordinates: [3, 6], orientation: North', () => {
+		const place =  { coordinates: [3, 6], orientation: 'East' }
+		const myRobot = createRobot([3, 6], 'North')
+		myRobot.turnRight()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance to right with coordinates: [7, 1], orientation: East', () => {
+		const place =  { coordinates: [7, 1], orientation: 'South' }
+		const myRobot = createRobot([7, 1], 'East')
+		myRobot.turnRight()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance to right with coordinates: [6, 1], orientation: South', () => {
+		const place =  { coordinates: [9, 4], orientation: 'West' }
+		const myRobot = createRobot([9, 4], 'South')
+		myRobot.turnRight()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance to right with coordinates: [1, 4], orientation: West', () => {
+		const place =  { coordinates: [1, 4], orientation: 'North' }
+		const myRobot = createRobot([1, 4], 'West')
+		myRobot.turnRight()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+})
+
+describe('robot turn to left', () => {
+	test('Should advance to left with coordinates: [4, 7], orientation: South', () => {
+		const place =  { coordinates: [4, 7], orientation: 'East' }
+		const myRobot = createRobot([4, 7], 'South')
+		myRobot.turnLeft()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+   
+	test('Should advance to left with coordinates: [1, 5], orientation: East', () => {
+		const place =  { coordinates: [1, 5], orientation: 'North' }
+		const myRobot = createRobot([1, 5], 'East')
+		myRobot.turnLeft()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance to left with coordinates: [7, 3], orientation: North', () => {
+		const place =  { coordinates: [7, 3], orientation: 'West' }
+		const myRobot = createRobot([7, 3], 'North')
+		myRobot.turnLeft()
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance to left with coordinates: [6, 1], orientation: West', () => {
+		const place =  {coordinates: [6, 1], orientation: 'South'}
+		const myRobot = createRobot([6, 1], 'West')
+		myRobot.turnLeft() 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+})
+
+describe('Instructions', () => {
+	test('Should show message invalid orientation with instruction not valid and with coordinates: [6, 1], orientation: West', () => {
+		const message = 'invalid orientation'
+		const myRobot = createRobot([6, 1], 'West')
+    myRobot.instructions('C')
+		const result = myRobot.getPosition()
+		expect(result).toBe(message)
+	})
+
+	test('Should advance with instruction R and with coordinates: [7, 3], orientation: North', () => {
+		const place =  { coordinates: [7, 3], orientation: 'East' }
+		const myRobot = createRobot([7, 3], 'North')
+		myRobot.instructions('R') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction R and with coordinates: [6, 1], orientation: East', () => {
+		const place =  { coordinates: [6, 1], orientation: 'South' }
+		const myRobot = createRobot([6, 1], 'East')
+		myRobot.instructions('R') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+  
+	test('Should advance with instruction R and with coordinates: [3, 9], orientation: South', () => {
+		const place =  { coordinates: [3 ,9], orientation: 'West' }
+		const myRobot = createRobot([3, 9], 'South')
+		myRobot.instructions('R') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction R and with coordinates: [1, 3], orientation: West', () => {
+		const place =  { coordinates: [1, 3], orientation: 'North' }
+		const myRobot = createRobot([1, 3], 'West')
+		myRobot.instructions('R') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction L and with coordinates: [3, 8], orientation: North', () => {
+		const place =  { coordinates: [3, 8], orientation: 'West' }
+		const myRobot = createRobot([3, 8], 'North')
+		myRobot.instructions('L') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction L and with coordinates: [2, 7], orientation: East', () => {
+		const place =  { coordinates: [2, 7], orientation: 'North' }
+		const myRobot = createRobot([2, 7],'East')
+		myRobot.instructions('L') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction L and with coordinates: [9, 7], orientation: South', () => {
+		const place =  { coordinates: [9, 7], orientation: 'East' }
+		const myRobot = createRobot([9, 7], 'South')
+		myRobot.instructions('L') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction L and with coordinates: [7, 4], orientation: West', () => {
+		const place =  { coordinates: [7, 4], orientation: 'South' }
+		const myRobot = createRobot([7, 4], 'West')
+		myRobot.instructions('L') 
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction A and with coordinates: [2,1], orientation: North', () => {
+		const place =  { coordinates: [2,2], orientation: 'North' }
+		const myRobot = createRobot([2,1], 'North')
+		myRobot.instructions('A')
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction A and with coordinates: [3, 2], orientation: East', () => {
+		const place =  { coordinates: [4, 2], orientation: 'East' }
+		const myRobot = createRobot([3, 2], 'East')
+		myRobot.instructions('A')
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction A and with coordinates: [9, 4], orientation: South', () => {
+		const place =  { coordinates: [9, 3], orientation: 'South' }
+		const myRobot = createRobot([9, 4], 'South')
+		myRobot.instructions('A')
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+
+	test('Should advance with instruction A and with coordinates: [9, 2], orientation: West', () => {
+		const place =  {coordinates: [8, 2], orientation: 'West'}
+		const myRobot = createRobot([9, 2], 'West')
+		myRobot.instructions('A')
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+})
+
+describe('robot function complete', () => {
+	test('Should function createdRobot with instruction RAALAL and with coordinates: [7, 3], orientation: North', () => {
+		const place =  {coordinates: [9, 4], orientation: 'West'}
+		const myRobot = createRobot([7, 3], 'North')
+		myRobot.instructions('RAALAL')
+		const result = myRobot.getPosition()
+		expect(result).toMatchObject(place)
+	})
+})
